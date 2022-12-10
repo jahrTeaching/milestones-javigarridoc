efc6067e837bb43e061b452926c0451347c24af7
javigarridoc <113106024+javigarridoc@users.noreply.github.com>
2022-12-10 10:14:14 +0100

    Add files via upload

--- sources/Functions/Physics.py
+++ sources/Functions/Physics.py
@@ -2,12 +2,13 @@ from numpy import array
 
 def Kepler(U,t):
     
-    F0 = U[2] # dxdt
-    F1 = U[3] # dydt
-    F2 = -U[0]/(U[0]**2 + U[1]**2)**(3/2) # -x/(x^2+y^2)3/2
-    F3 = -U[1]/(U[0]**2 + U[1]**2)**(3/2) # -y/(x^2+y^2)3/2
+    x = U[0]; 
+    y = U[1]; 
+    dxdt = U[2]; 
+    dydt = U[3]
+    d = (x**2 + y**2)**(3/2)
 
-    return array([F0, F1, F2, F3])
+    return array([ dxdt, dydt, -x/d, -y/d])
 
 def Linear_Oscilator(U, t):
     r = U[0]
