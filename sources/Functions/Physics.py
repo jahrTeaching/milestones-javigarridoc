d855a7a81423d388778b0dddf5fb26938753c5ad
javigarridoc <113106024+javigarridoc@users.noreply.github.com>
2022-12-10 10:14:43 +0100

    Add files via upload

--- sources/Functions/Physics.py
+++ sources/Functions/Physics.py
@@ -2,9 +2,9 @@ from numpy import array
 
 def Kepler(U,t):
     
-    x = U[0]; 
-    y = U[1]; 
-    dxdt = U[2]; 
+    x = U[0]
+    y = U[1]
+    dxdt = U[2]
     dydt = U[3]
     d = (x**2 + y**2)**(3/2)
 
